770434a0fa4e4779e499361aff546e0917fdcbfc
Yousef Roowala <yroowalar@gmail.com>
2024-02-14 20:30:16 +0400

    removed comments from script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// 1 meter = 3.281 feet
-// 1 liter = 0.264 gallon
-// 1 kilogram = 2.204 pound
-
 const inputEl = document.getElementById("input-el")
 const convertBtn = document.getElementById("convert-btn")
 const resultMF = document.getElementById("result-mf")
